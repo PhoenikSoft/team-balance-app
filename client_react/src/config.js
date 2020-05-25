1a20a714418e9f4583dce90a3df60559b1c0ea36
Yevhen Karychkovskyi <karichkovsky@yandex.com>
2020-05-25 13:37:14 +0200

    Dockerize React client

--- client_react/src/config.js
+++ client_react/src/config.js
@@ -1,5 +1,5 @@
-const prod_url = "http://teambalanceapp-env.eba-pv93a9qx.eu-central-1.elasticbeanstalk.com";
-const dev_url = "http://192.168.0.103:8080";
+const prod_url = "";
+const dev_url = "http://localhost:8080";
 
 export default {
     apiUrl: process.env.NODE_ENV !== 'production' ? dev_url : prod_url
