411479040305170cca7f084112b63a0017088cf3
Yevhen Batsiun <yevhen.batsiun@sisense.com>
2020-06-07 14:05:30 +0300

    config fix

--- client_react/src/config.js
+++ client_react/src/config.js
@@ -1,5 +1,5 @@
 const prod_url = "";
-const dev_url = "http://192.168.0.108:8080";
+const dev_url = "http://localhost:8080";
 
 export default {
     apiUrl: process.env.NODE_ENV !== 'production' ? dev_url : prod_url
