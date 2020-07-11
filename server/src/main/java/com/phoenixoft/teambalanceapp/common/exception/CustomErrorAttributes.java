33483942b29fd927b06955686a1764cb416a7c15
Yevhen Karychkovskyi <karichkovsky@yandex.com>
2020-07-11 13:17:35 +0200

    Remove trace and exception info from internal error response

---
+++ server/src/main/java/com/phoenixoft/teambalanceapp/common/exception/CustomErrorAttributes.java
@@ -0,0 +1,18 @@
+package com.phoenixoft.teambalanceapp.common.exception;
+
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Map;
+
+@Component
+public class CustomErrorAttributes extends DefaultErrorAttributes {
+
+    @Override
+    public Map<String, Object> getErrorAttributes(WebRequest webRequest, boolean includeStackTrace) {
+        Map<String, Object> errorAttributes = super.getErrorAttributes(webRequest, includeStackTrace);
+        errorAttributes.remove("message");
+        return errorAttributes;
+    }
+}
