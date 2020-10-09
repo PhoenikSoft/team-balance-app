2ff83e282cecc7c03baf5ce372c1315346f461a2
Yevhen <karichkovsky@yandex.com>
2020-10-09 16:33:30 +0200

    MIgrate Quartz tables to a separate schema (#66)

--- server/src/main/java/com/phoenixoft/teambalanceapp/config/QuartzConfig.java
+++ server/src/main/java/com/phoenixoft/teambalanceapp/config/QuartzConfig.java
@@ -2,9 +2,16 @@ package com.phoenixoft.teambalanceapp.config;
 
 import com.phoenixoft.teambalanceapp.vote.job.FinishGameVotingJob;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseDataSource;
+import org.springframework.boot.autoconfigure.quartz.QuartzDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.scheduling.quartz.JobDetailFactoryBean;
+
+import javax.sql.DataSource;
 
 @Configuration
 @RequiredArgsConstructor
@@ -23,5 +30,20 @@ public class QuartzConfig {
         jobDetailFactory.setDurability(true);
         jobDetailFactory.setRequestsRecovery(true);
         return jobDetailFactory;
+    }
+
+    @Bean
+    @Primary
+    @LiquibaseDataSource
+    @ConfigurationProperties("spring.datasource")
+    DataSource springDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    @QuartzDataSource
+    @ConfigurationProperties("quartz.datasource")
+    DataSource quartzDataSource() {
+        return DataSourceBuilder.create().build();
     }
 }
