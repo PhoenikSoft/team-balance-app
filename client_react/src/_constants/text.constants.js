e252dd489a852906b6901faa95328275be97be19
Yevhen <ybatsiun@gmail.com>
2020-10-14 14:35:43 +0300

    localization initi commit

---
+++ client_react/src/_constants/text.constants.js
@@ -0,0 +1,16 @@
+import LocalizedStrings from 'react-localization';
+
+export default new LocalizedStrings({
+    en: {
+        how: "How do you want your egg today?",
+        boiledEgg: "Boiled egg",
+        softBoiledEgg: "Soft-boiled egg",
+        choice: "How to choose the egg"
+    },
+    ua: {
+        how: "Come vuoi il tuo uovo oggi?",
+        boiledEgg: "Uovo sodo",
+        softBoiledEgg: "Uovo alla coque",
+        choice: "Come scegliere l'uovo"
+    }
+});
