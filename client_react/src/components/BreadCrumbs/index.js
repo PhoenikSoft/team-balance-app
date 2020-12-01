79d077dd71ba78b8fad8e574d13ba12bf8668770
Yevhen <ybatsiun@gmail.com>
2020-12-02 01:22:44 +0200

    comments

--- client_react/src/components/BreadCrumbs/index.js
+++ client_react/src/components/BreadCrumbs/index.js
@@ -10,12 +10,11 @@ const mapStateToProps = state => {
     return {
         // TODO to show groupname: if now group name make additional fetch to get this name
         // TODO to avoide passing if only one is set, make an additional field in state (SELECTED_GROUP/GAME)
-        // and pass it here
+        // and pass it here, clean those if redirected to (Home or Group pages)
         groupName: state.groups.length === 1 ? state.groups[0]?.name : '',
         groupId: state.groups[0]?.id,
         gameName: state.game.name ? state.game.name : '',
-        // Hack to make this component rerender when url changes. To remove this you need to delete group and/or game
-        // from state when navigating to a different page (Home or Group pages)
+        // Hack to make this component rerender when url changes. To remove this see prev TODO
         url: document.URL
     }
 };
